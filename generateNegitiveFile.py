f2c1eec4bcfb44330a2f2793159e656bc0ac9416
hamish <user@asp.net>
2021-03-19 11:42:00 +1100

    minor + better testing setup

--- generateNegitiveFile.py
+++ generateNegitiveFile.py
@@ -7,8 +7,8 @@ class generateNegitiveFile:
         # open the output file for writing. will overwrite all existing data in there
         with open('neg.txt', 'w') as f:
             # loop over all the filenames
-            for filename in os.listdir('planes/not'):
-                f.write('planes/not/' + filename + '\n')
+            for filename in os.listdir('Images/empty_sorted/'):
+                f.write('Images/empty_sorted/' + filename + '\n')
 
 ####
 
